0e7a244468e26625633a99544d16728088144073
zulzayn <zulwaqarzain96@gmail.com>
2020-02-22 17:33:44 +0800

    avail

--- app/Http/Controllers/Api/SASController.php
+++ app/Http/Controllers/Api/SASController.php
@@ -39,73 +39,83 @@ class SASController extends Controller
     {
         $managers = Role::where('level' , 1)->first();
         
-        $managers = $managers->user;
-
-        if(count($managers) != 0)
+        if($managers)
         {
-            $managers = $managers->user;
-
-            $managers_array = [];
         
-            foreach ($managers as $key => $manager) {
-                array_push($managers_array, $manager->id);
-            }
-    
-            $request->validate([
-                'start_date'        => 'required',
-                'start_time'        => 'required',
-                'end_date'          => 'required', 
-                'end_time'          => 'required', 
-                'job_number'        => 'required',
-                'job_title'         => 'required',
-                'job_description'   => 'required', 
-                'assign_staff.*'    => 'required',
-            ]);
-    
-                $add = New SAS;
-                $add->id = Uuid::uuid4()->getHex();
-                $add->job_number = $request->job_number;
-                $add->job_title = $request->job_title;
-                $add->job_description = $request->job_description;
-                $add->status = 'In Progress';
-                $add->id_approver = json_encode($managers_array);
-                $add->approval_status = 'Submitted for Approval';
-                $add->created_by = auth()->user()->id;
-                $add->save();
-    
-               foreach ($request->assign_staff as $key => $assign_staff) {
-                    $add2 = New SASStaffAssign;
-                    $add2->id = Uuid::uuid4()->getHex();
-                    $add2->id_user = $assign_staff;
-                    $add2->id_sas = $add->id;
-                    $add2->start_date = ''.date("Y-m-d", strtotime($request->start_date)).' '.date("H:i:s", strtotime($request->start_time)).'';
-                    $add2->end_date = ''.date("Y-m-d", strtotime($request->end_date)).' '.date("H:i:s", strtotime($request->end_time)).'';
-                    $add2->status = "0";
-                    $add2->created_by = auth()->user()->id;
-                    $add2->save();  
-               }
-
-            foreach ($managers as $key => $manager) {
-                $noti = New Notification;
-                $noti->id = Uuid::uuid4()->getHex();
-                $noti->to_user = $manager->id;
-                $noti->tiny_img_url = '';
-                $noti->title = '[APPROVAL] SAS : '.$add->job_number.' need approval from you.';
-                $noti->desc = 'SAS : '.$add->job_number.' need approval from you.';
-                $noti->type = 'A';
-                $noti->click_url = '';
-                $noti->send_status = 'P';
-                $noti->status = '';
-                $noti->created_by = auth()->user()->id;
-                $noti->save();
-            }
-    
-            return response(['status' => 'OK' , 'message' => 'Successfully add new task']);
+            return response(['status' => 'OK' , 'message' => 'No managers found in system, please register at least a manager to get their approval.']);
+        
         }
         else
         {
-            return response(['status' => 'OK' , 'message' => 'No managers found in system, please register at least a manager to get their approval.']);
+            $managers = $managers->user;
+
+            if(count($managers) != 0)
+            {
+                $managers = $managers->user;
+
+                $managers_array = [];
+            
+                foreach ($managers as $key => $manager) {
+                    array_push($managers_array, $manager->id);
+                }
+        
+                $request->validate([
+                    'start_date'        => 'required',
+                    'start_time'        => 'required',
+                    'end_date'          => 'required', 
+                    'end_time'          => 'required', 
+                    'job_number'        => 'required',
+                    'job_title'         => 'required',
+                    'job_description'   => 'required', 
+                    'assign_staff.*'    => 'required',
+                ]);
+        
+                    $add = New SAS;
+                    $add->id = Uuid::uuid4()->getHex();
+                    $add->job_number = $request->job_number;
+                    $add->job_title = $request->job_title;
+                    $add->job_description = $request->job_description;
+                    $add->status = 'In Progress';
+                    $add->id_approver = json_encode($managers_array);
+                    $add->approval_status = 'Submitted for Approval';
+                    $add->created_by = auth()->user()->id;
+                    $add->save();
+        
+                foreach ($request->assign_staff as $key => $assign_staff) {
+                        $add2 = New SASStaffAssign;
+                        $add2->id = Uuid::uuid4()->getHex();
+                        $add2->id_user = $assign_staff;
+                        $add2->id_sas = $add->id;
+                        $add2->start_date = ''.date("Y-m-d", strtotime($request->start_date)).' '.date("H:i:s", strtotime($request->start_time)).'';
+                        $add2->end_date = ''.date("Y-m-d", strtotime($request->end_date)).' '.date("H:i:s", strtotime($request->end_time)).'';
+                        $add2->status = "0";
+                        $add2->created_by = auth()->user()->id;
+                        $add2->save();  
+                }
+
+                foreach ($managers as $key => $manager) {
+                    $noti = New Notification;
+                    $noti->id = Uuid::uuid4()->getHex();
+                    $noti->to_user = $manager->id;
+                    $noti->tiny_img_url = '';
+                    $noti->title = '[APPROVAL] SAS : '.$add->job_number.' need approval from you.';
+                    $noti->desc = 'SAS : '.$add->job_number.' need approval from you.';
+                    $noti->type = 'A';
+                    $noti->click_url = '';
+                    $noti->send_status = 'P';
+                    $noti->status = '';
+                    $noti->created_by = auth()->user()->id;
+                    $noti->save();
+                }
+        
+                return response(['status' => 'OK' , 'message' => 'Successfully add new task']);
+            }
+            else
+            {
+                return response(['status' => 'OK' , 'message' => 'No managers found in system, please register at least a manager to get their approval.']);
+            }
         }
+        
        
     }
 
@@ -146,8 +156,8 @@ class SASController extends Controller
     
     public function getAvailableStaff($datefrom, $dateto)
     {
-        $unavailableStaffs = SASStaffAssign::where('start_date' , '>=' , $datefrom)
-        ->where('end_date' , '<=' , $dateto)
+        $unavailableStaffs = SASStaffAssign::where('start_date' , '>=' , date('Y-m-d H:i:s' , $datefrom))
+        ->where('end_date' , '<=' , date('Y-m-d H:i:s' , $dateto))
         ->get();
 
         $availableUsers = array();
