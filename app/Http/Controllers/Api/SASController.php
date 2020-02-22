9d9761c5fb0c68eb9bc6e5f751b18b566c72a63f
zulzayn <zulwaqarzain96@gmail.com>
2020-02-22 22:59:56 +0800

    sas show

--- app/Http/Controllers/Api/SASController.php
+++ app/Http/Controllers/Api/SASController.php
@@ -127,7 +127,9 @@ class SASController extends Controller
      */
     public function show($id)
     {
-        //
+        $sas = SAS::with('SASStaffAssign.SASComment')->whereIn('id', $id)->get();
+
+        return response(['status' => 'OK' , 'message' =>  $sas]); 
     }
 
     /**
