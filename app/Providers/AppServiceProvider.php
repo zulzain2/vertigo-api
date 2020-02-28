9f86b867348fe70e457da57362bdae9c07ed418b
zulzayn <zulwaqarzain96@gmail.com>
2020-02-28 18:11:47 +0800

    appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +24,16 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    // public function boot()
+    // {
+    //     Schema::defaultStringLength(191);
+    // }
+
+    public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS'))
+        {
+            $url->forceSchema('https');
+        }
     }
 }
