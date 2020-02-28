86859582631539364e14338100e478a52cb7eb21
zulzayn <zulwaqarzain96@gmail.com>
2020-02-28 18:06:21 +0800

    fcm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,11 @@
        <!-- Fonts -->
        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+        <script src="https://www.gstatic.com/firebasejs/7.9.2/firebase-app.js"></script>
+
+        <script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js"></script>
+
        <link rel="manifest" href="manifest.json">
 
 
@@ -100,53 +105,7 @@
             </div>
         </div>
 
-        {{-- <script src="{{asset('js/firebase.js')}}"></script> --}}
-
-
-        <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="https://www.gstatic.com/firebasejs/7.9.2/firebase-app.js"></script>
-
-        <script src="https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js"></script>
+        <script src="{{asset('js/firebase.js')}}"></script>
         
-        <script>
-            // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBwzH3ORW0DB6wWaJ6y0YILgoE8qukOOco",
-    authDomain: "vertigo-2020.firebaseapp.com",
-    databaseURL: "https://vertigo-2020.firebaseio.com",
-    projectId: "vertigo-2020",
-    storageBucket: "vertigo-2020.appspot.com",
-    messagingSenderId: "15333066764",
-    appId: "1:15333066764:web:335016e9c10981f2980629",
-    measurementId: "G-V22MJ22RQP"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
- // Retrieve Firebase Messaging object.
- const messaging = firebase.messaging();
-        messaging
-            .requestPermission()
-            .then(function () {
-
-                console.log("Notification permission granted.");
-                return messaging.getToken()
-
-            }).then(function(token){
-
-                console.log(token)
-            }).
-
-            catch(function (err) {
-
-                console.log("Unable to get permission to notify." , err);
-            });
-
-messaging.onMessage((payload) => {
-
-    console.log(payload);
-})
-        </script>
     </body>
 </html>
