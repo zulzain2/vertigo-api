2ea72f70d5ae2a2e2064a6c48b1e6ab20b5c3f68
zulzayn <zulwaqarzain96@gmail.com>
2020-02-29 21:42:02 +0800

    try 2 with ebs index

--- app/Http/Controllers/Api/EBSController.php
+++ app/Http/Controllers/Api/EBSController.php
@@ -21,7 +21,7 @@ class EBSController extends Controller
      */
     public function index()
     {
-        $ebs = EBS::with('EBSStaffUse.EBSEquipmentUse')->get();
+        $ebs = EBS::with('EBSStaffUse')->with('EBSEquipmentUse')->get();
 
         return response(['status' => 'OK' , 'message' =>  $ebs]); 
     }
