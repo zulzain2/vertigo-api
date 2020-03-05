5b5c3956845f084d8242d1a2a4b6c2ef1d060aa0
zulzayn <zulwaqarzain96@gmail.com>
2020-03-05 11:54:55 +0800

    fcm test

--- app/Notification.php
+++ app/Notification.php
@@ -97,37 +97,61 @@ class Notification extends Model
 
     }
 
-    // public static function numberAlert() 
-    // {
-
-    // }
-
     public static function notificationFCM($json_data)
     {
-        $data = json_encode($json_data);
-        //FCM API end-point
-        $url = 'https://fcm.googleapis.com/fcm/send';
-        //api_key in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
-        // $server_key = env('FCM_SERVER_KEY');
-        $server_key = 'AIzaSyBMGQ9NcOOklBn5dbYgSlERFe9ayGb1xek';
-        //header with content_type api key
+        // $data = json_encode($json_data);
+        // //FCM API end-point
+        // $url = 'https://fcm.googleapis.com/fcm/send';
+        // //api_key in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
+        // // $server_key = env('FCM_SERVER_KEY');
+        // $server_key = 'AAAAA5HsCAw:APA91bGX1b5j9YnR3U9WTbVfAfG0l9qX3WZPPzqvAG32T8C7OCggurbsbOey3UvafiygMVefuHxg36UA9L8ouISpZZ7I6FuAQWUFUrun323YDXOSrCZwflHM4LYxR4CpM45KloNcCAdB';
+        // //header with content_type api key
+        // $headers = array(
+        //     'Content-Type:application/json',
+        //     'Authorization:key='.$server_key
+        // );
+        // //CURL request to route notification to FCM connection server (provided by Google)
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $url);
+        // curl_setopt($ch, CURLOPT_POST, true);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        // $result = curl_exec($ch);
+        // if ($result === FALSE) {
+        //     die('Oops! FCM Send Error: ' . curl_error($ch));
+        // }
+        // curl_close($ch);
+
+        $payload = array(
+                'to'=>'/topics/my-app',
+                'priority'=>'high',
+                "mutable_content"=>true,
+                "notification"=>array(
+                            "title"=> $_POST['title'],
+                            "body"=> $_POST['summary']
+                ),
+                'data'=>array(
+                    'action'=>'models',
+                    'model_id'=>'2701',
+                    )
+            );
         $headers = array(
-            'Content-Type:application/json',
-            'Authorization:key='.$server_key
+            'Authorization:key=AAAAA5HsCAw:APA91bGX1b5j9YnR3U9WTbVfAfG0l9qX3WZPPzqvAG32T8C7OCggurbsbOey3UvafiygMVefuHxg36UA9L8ouISpZZ7I6FuAQWUFUrun323YDXOSrCZwflHM4LYxR4CpM45KloNcCAdB',
+            'Content-Type: application/json'
         );
-        //CURL request to route notification to FCM connection server (provided by Google)
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $result = curl_exec($ch);
-        if ($result === FALSE) {
-            die('Oops! FCM Send Error: ' . curl_error($ch));
-        }
-        curl_close($ch);
+        curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+        curl_setopt( $ch,CURLOPT_POST, true );
+        curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+        curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $payload ) );
+        $result = curl_exec($ch );
+        curl_close( $ch );
+        var_dump($result);exit;
+  
     }
 }
