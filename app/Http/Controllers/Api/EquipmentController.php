79ba6287e429f59558648b911da866b782a7e4c0
zulzayn <zulwaqarzain96@gmail.com>
2020-03-08 13:58:02 +0800

    =

--- app/Http/Controllers/Api/EquipmentController.php
+++ app/Http/Controllers/Api/EquipmentController.php
@@ -87,7 +87,7 @@ class EquipmentController extends Controller
      */
     public function show($id)
     {
-        $equipment = Equipment::find($id);
+        $equipment = Equipment::with('equipmentcategory')->whereIn('id', $id)->get();
 
         return response()->json(['status' => 'OK' , 'equipment' => $equipment]);
     }
