fbfec3783685e9389ff8c0a69095bfc9c20a70d6
zulzayn <zulwaqarzain96@gmail.com>
2020-04-01 11:35:58 +0800

    sas

--- app/SAS.php
+++ app/SAS.php
@@ -10,7 +10,7 @@ class SAS extends Model
     use Notifiable;
     protected $table = 'sas';
     public $incrementing = FALSE;
-    protected $with = ['sasstaffassign.sascomment.user'];
+    protected $with = ['sasstaffassign.sascomment', 'sasstaffassign.user'];
 
     public function sasstaffassign() {
         return $this->hasMany('App\SASStaffAssign', 'id_sas', 'id');
