e2cecc9a9217020e8cdf74e3b7066ac0b7f578c5
zulzayn <zulwaqarzain96@gmail.com>
2020-04-07 21:13:42 +0800

    =

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://vertigoapi.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
