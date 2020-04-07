4f3b2fc509b86f53dca2d08ced0ad08cbd78f2f6
zulzayn <zulwaqarzain96@gmail.com>
2020-04-07 21:43:56 +0800

    =

--- app/Http/Middleware/HttpsRedirect.php
+++ app/Http/Middleware/HttpsRedirect.php
@@ -16,8 +16,9 @@ class HttpsRedirect
     public function handle($request, Closure $next)
     {
         if(!$request->secure() && env('APP_ENV') === 'production') {
-            return redirect()->secure($request->getRequestUri()); 
             dd(redirect()->secure($request->getRequestUri()));
+            return redirect()->secure($request->getRequestUri()); 
+            
         }
        
         return $next($request);
