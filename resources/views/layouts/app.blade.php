29f7313c8080df7910d7f00eb91893a07e747bd5
zulzayn <zulwaqarzain96@gmail.com>
2020-04-09 15:16:21 +0800

    Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('img/logoMain2.png') }}">
     <title>Vertigo</title>
     <!-- Bootstrap Core CSS -->
     <link href="{{ url('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
