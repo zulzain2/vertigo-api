7ab7181065501b02de13b8d734667b0c41f9ea7d
ejulfaey <ejulfae@gmail.com>
2020-07-08 19:18:10 +0800

    update sas

--- app/Http/Resources/Calendar/SAS/SAS.php
+++ app/Http/Resources/Calendar/SAS/SAS.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Calendar\SAS;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\User as UserResource;
 
 class SAS extends JsonResource
 {
@@ -18,11 +19,14 @@ class SAS extends JsonResource
 
         return [
             'id' => $this->sas->id,
+            'start_date' => date('Y-m-d', strtotime($this->start_date)),
+            'end_date' => date('Y-m-d', strtotime($this->end_date)),
             'start_time' => $time['start_time'],
             'end_time' => $time['end_time'],
             'job_number' => $this->sas->job_number,
             'job_title' => $this->sas->job_title,
             'status' => $this->status,
+            // 'person_in_use' => UserResource::collection($this->)
         ];
     }
 }
