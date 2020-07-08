c76766e0c95b1d545028dab7de3ad90cb853b572
ejulfaey <ejulfae@gmail.com>
2020-07-08 20:22:47 +0800

    updated api:calendar sas

--- app/Http/Resources/Calendar/SAS/SAS.php
+++ app/Http/Resources/Calendar/SAS/SAS.php
@@ -26,7 +26,7 @@ class SAS extends JsonResource
             'job_number' => $this->sas->job_number,
             'job_title' => $this->sas->job_title,
             'status' => $this->status,
-            'person_in_use' => UserResource::collection($this->sas->personInUse),
+            'person_in_use' => UserResource::collection($this->sas->sasstaffassign),
         ];
     }
 }
