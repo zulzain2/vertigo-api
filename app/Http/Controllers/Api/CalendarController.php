0a51bb8d756c8901cc7045300826fae9c4ac68f9
ejulfaey <ejulfae@gmail.com>
2020-07-10 11:39:53 +0800

    updated api: calendar tms

--- app/Http/Controllers/Api/CalendarController.php
+++ app/Http/Controllers/Api/CalendarController.php
@@ -49,6 +49,7 @@ class CalendarController extends Controller
     public function listTMS(Request $request)
     {
         $tenders = TMS::whereMonth('sitevisit_start_date', $request->month)
+            ->whereState('Approve')
             ->groupBy('vtsb_num')
             ->get();
 
