d6b8925c491d91e5be4dee3d2428a4cf608678e5
ZulZayn <zulwaqarzain96@gmail.com>
2020-10-29 19:44:30 +0800

    preloader ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -352,8 +352,8 @@
                 <!-- ============================================================== -->
                 <!-- Preloader - style you can find in spinners.css -->
                 <!-- ============================================================== -->
-                <div class="preloader">
-                    <div class="loader">
+                <div class="preloader" style="margin-left: -25px;top: unset;">
+                    <div class="loader" style="left: 45%;">
                         <div class="loader__figure"></div>
                         <p class="loader__label">Vertigo</p>
                     </div>
